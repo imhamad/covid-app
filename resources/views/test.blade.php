b1c76e651b649e118faedc0d8e44b4251d9f1a74
imhamad <hamadofficial@yahoo.com>
2020-07-17 13:46:13 +0500

    [CORE] -- ADDED

---
+++ resources/views/test.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome to Laravel</title>
+    <style>
+        .text-center{
+            padding: 2em;
+            margin: auto;
+            display: block;
+            font-size: 40px;
+            font-family: "open sans",serif;
+            min-width: 100%;
+        }
+    </style>
+</head>
+<body>
+
+    <div class="text-center">
+COVID 2019 Experience on Your Life, Job and Routine
+    </div>
+
+</body>
+</html>
